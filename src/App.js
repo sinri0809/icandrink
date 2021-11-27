792e2eb93caeecd6694f1bcc51b46a843e7178da
sinri0809 <1lliodqb@gmail.com>
2021-11-27 20:22:56 +0900

    초기 환경 세팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
+/* eslint-disable */
+// import logo from './logo.svg';
 import './App.css';
+import './style/style.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      hi
+      <p className="temp">hi</p>
+      <p className="temp2">hi</p>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +22,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
